4833727b71cfa3659564d706ed9ea380434d4e68
overlookmotel <theoverlookmotel@gmail.com>
2018-07-21 17:12:54 +0100

    Tests refactor WIP 1

--- test/all.test.js
+++ test/all.test.js
@@ -58,189 +58,82 @@ describe('Constructor', function() {
 
 describe('`.plugin()`', function() {
 	beforeEach(function() {
+		this.pluginName = 'plugName';
+		this.pluginName2 = 'plugName2';
 		this.globalOptions = {globalOpt: 1};
 	});
 
 	describe('with arguments (name, plugin, options)', function() {
 		beforeEach(function() {
-			this.app = new Pluggi({plugName: this.globalOptions});
+			this.app = new Pluggi({[this.pluginName]: this.globalOptions});
 			this.options = {localOpt: 2};
 			this.res = {resProp: 3};
 			this.plugin = sinon.fake.returns(this.res);
-			this.ret = this.app.plugin('plugName', this.plugin, this.options);
+			this.ret = this.app.plugin(this.pluginName, this.plugin, this.options);
+			this.app.plugin(this.pluginName2, () => {}, this.options);
 		});
 
-		it('calls plugin', function() {
-			expect(this.plugin).to.be.calledOnce;
-		});
-
-		it('calls plugin with 2 args', function() {
-			expectCalledWithTwoArgs(this.plugin);
-		});
-
-		it('calls plugin with app', function() {
-			expectCalledWithFirstArg(this.plugin, this.app);
-		});
-
-		it('calls plugin with provided options', function() {
-			expectCalledWithSecondArg(this.plugin, sinon.match(this.options));
-		});
-
-		it('calls plugin with global options', function() {
-			expectCalledWithSecondArg(this.plugin, sinon.match(this.globalOptions));
-		});
-
-		it('records plugin return value in plugins object', function() {
-			expect(this.app.plugins.plugName).to.equal(this.res);
-		});
-
-		it('when plugin returns undefined, records empty object in plugins object', function() {
-			this.app.plugin('plugName2', () => {}, this.options);
-			expect(this.app.plugins.plugName2).to.deep.equal({});
-		});
-
-		it('returns `app` for chaining', function() {
-			expect(this.ret).to.equal(this.app);
-		});
+		pluginTests({localOptions: true});
 	});
 
 	describe('with arguments (name, plugin)', function() {
 		beforeEach(function() {
-			this.app = new Pluggi({plugName: this.globalOptions});
+			this.app = new Pluggi({[this.pluginName]: this.globalOptions});
 			this.res = {resProp: 3};
 			this.plugin = sinon.fake.returns(this.res);
 			this.ret = this.app.plugin('plugName', this.plugin);
+			this.app.plugin(this.pluginName2, () => {});
 		});
 
-		it('calls plugin', function() {
-			expect(this.plugin).to.be.calledOnce;
-		});
-
-		it('calls plugin with 2 args', function() {
-			expectCalledWithTwoArgs(this.plugin);
-		});
-
-		it('calls plugin with app', function() {
-			expectCalledWithFirstArg(this.plugin, this.app);
-		});
-
-		it('calls plugin with global options', function() {
-			expectCalledWithSecondArg(this.plugin, this.globalOptions);
-		});
-
-		it('records plugin return value in plugins object', function() {
-			expect(this.app.plugins.plugName).to.equal(this.res);
-		});
-
-		it('records empty object in plugins object when plugin returns undefined', function() {
-			this.app.plugin('plugName2', () => {});
-			expect(this.app.plugins.plugName2).to.deep.equal({});
-		});
-
-		it('returns `app` for chaining', function() {
-			expect(this.ret).to.equal(this.app);
-		});
+		pluginTests({localOptions: false});
 	});
 
 	describe('with arguments (plugin, options)', function() {
 		beforeEach(function() {
+			this.pluginName = 'proxy';
 			// NB name of function returned by `sinon.fake()` is 'proxy'
-			this.app = new Pluggi({proxy: this.globalOptions});
+			this.app = new Pluggi({[this.pluginName]: this.globalOptions});
 			this.options = {localOpt: 2};
 			this.res = {resProp: 3};
 			this.plugin = sinon.fake.returns(this.res);
 			this.ret = this.app.plugin(this.plugin, this.options);
-		});
-
-		it('calls plugin', function() {
-			expect(this.plugin).to.be.calledOnce;
-		});
-
-		it('calls plugin with 2 args', function() {
-			expectCalledWithTwoArgs(this.plugin);
-		});
-
-		it('calls plugin with app', function() {
-			expectCalledWithFirstArg(this.plugin, this.app);
-		});
-
-		it('calls plugin with provided options', function() {
-			expectCalledWithSecondArg(this.plugin, sinon.match(this.options));
-		});
-
-		it('calls plugin with global options', function() {
-			expectCalledWithSecondArg(this.plugin, sinon.match(this.globalOptions));
-		});
-
-		it('records plugin return value in plugins object', function() {
-			expect(this.app.plugins.proxy).to.equal(this.res);
-		});
-
-		it('records empty object in plugins object when plugin returns undefined', function() {
 			this.app.plugin(function plugName2() {}, this.options);
-			expect(this.app.plugins.plugName2).to.deep.equal({});
 		});
+
+		pluginTests({localOptions: true});
 
 		it('throws when plugin function is nameless', function() {
 			expect(() => {
 				this.app.plugin(function() {}, this.options);
 			}).to.throw(Error, /^name passed to `\.plugin\(\)` must be provided or plugin function must be named$/);
 		});
-
-		it('returns `app` for chaining', function() {
-			expect(this.ret).to.equal(this.app);
-		});
 	});
 
 	describe('with arguments (plugin)', function() {
 		beforeEach(function() {
+			this.pluginName = 'proxy';
 			// NB name of function returned by `sinon.fake()` is 'proxy'
-			this.app = new Pluggi({proxy: this.globalOptions});
+			this.app = new Pluggi({[this.pluginName]: this.globalOptions});
 			this.res = {resProp: 3};
 			this.plugin = sinon.fake.returns(this.res);
 			this.ret = this.app.plugin(this.plugin);
-		});
-
-		it('calls plugin', function() {
-			expect(this.plugin).to.be.calledOnce;
-		});
-
-		it('calls plugin with 2 args', function() {
-			expectCalledWithTwoArgs(this.plugin);
-		});
-
-		it('calls plugin with app', function() {
-			expectCalledWithFirstArg(this.plugin, this.app);
-		});
-
-		it('calls plugin with global options', function() {
-			expectCalledWithSecondArg(this.plugin, this.globalOptions);
-		});
-
-		it('records plugin return value in plugins object', function() {
-			expect(this.app.plugins.proxy).to.equal(this.res);
-		});
-
-		it('records empty object in plugins object when plugin returns undefined', function() {
 			this.app.plugin(function plugName2() {});
-			expect(this.app.plugins.plugName2).to.deep.equal({});
 		});
+
+		pluginTests({localOptions: false});
 
 		it('throws when plugin function is nameless', function() {
 			expect(() => {
 				this.app.plugin(function() {});
 			}).to.throw(Error, /^name passed to `\.plugin\(\)` must be provided or plugin function must be named$/);
 		});
-
-		it('returns `app` for chaining', function() {
-			expect(this.ret).to.equal(this.app);
-		});
 	});
 
 	describe('with arguments (name, options)', function() {
 		describe('with no PLUGIN_PREFIX defined', function() {
 			beforeEach(function() {
-				this.app = new Pluggi({spyModule: this.globalOptions});
+				this.pluginName = 'spyModule';
+				this.app = new Pluggi({[this.pluginName]: this.globalOptions});
 				this.options = {localOpt: 2};
 				this.res = spyModule.spyModuleReturnValue;
 				this.plugin = spyModule;
@@ -251,38 +144,13 @@ describe('`.plugin()`', function() {
 				spyModule.resetHistory();
 			});
 
-			it('calls plugin', function() {
-				expect(this.plugin).to.be.calledOnce;
-			});
-
-			it('calls plugin with 2 args', function() {
-				expectCalledWithTwoArgs(this.plugin);
-			});
-
-			it('calls plugin with app', function() {
-				expectCalledWithFirstArg(this.plugin, this.app);
-			});
-
-			it('calls plugin with provided options', function() {
-				expectCalledWithSecondArg(this.plugin, sinon.match(this.options));
-			});
-
-			it('calls plugin with global options', function() {
-				expectCalledWithSecondArg(this.plugin, sinon.match(this.globalOptions));
-			});
-
-			it('records plugin return value in plugins object', function() {
-				expect(this.app.plugins.spyModule).to.equal(this.res);
-			});
-
-			it('returns `app` for chaining', function() {
-				expect(this.ret).to.equal(this.app);
-			});
+			pluginTests({localOptions: true, skipEmptyObject: true});
 		});
 
 		describe('with PLUGIN_PREFIX defined', function() {
 			beforeEach(function() {
-				this.app = new Pluggi({'module': this.globalOptions});
+				this.pluginName = 'module';
+				this.app = new Pluggi({[this.pluginName]: this.globalOptions});
 				this.app[Pluggi.PLUGIN_PREFIX] = 'spy';
 				this.options = {localOpt: 2};
 				this.res = spyModule.spyModuleReturnValue;
@@ -294,40 +162,15 @@ describe('`.plugin()`', function() {
 				spyModule.resetHistory();
 			});
 
-			it('calls plugin', function() {
-				expect(this.plugin).to.be.calledOnce;
-			});
-
-			it('calls plugin with 2 args', function() {
-				expectCalledWithTwoArgs(this.plugin);
-			});
-
-			it('calls plugin with app', function() {
-				expectCalledWithFirstArg(this.plugin, this.app);
-			});
-
-			it('calls plugin with provided options', function() {
-				expectCalledWithSecondArg(this.plugin, sinon.match(this.options));
-			});
-
-			it('calls plugin with global options', function() {
-				expectCalledWithSecondArg(this.plugin, sinon.match(this.globalOptions));
-			});
-
-			it('records plugin return value in plugins object', function() {
-				expect(this.app.plugins.module).to.equal(this.res);
-			});
-
-			it('returns `app` for chaining', function() {
-				expect(this.ret).to.equal(this.app);
-			});
+			pluginTests({localOptions: true, skipEmptyObject: true});
 		});
 	});
 
 	describe('with arguments (name)', function() {
 		describe('with no PLUGIN_PREFIX defined', function() {
 			beforeEach(function() {
-				this.app = new Pluggi({spyModule: this.globalOptions});
+				this.pluginName = 'spyModule';
+				this.app = new Pluggi({[this.pluginName]: this.globalOptions});
 				this.res = spyModule.spyModuleReturnValue;
 				this.plugin = spyModule;
 				this.ret = this.app.plugin('spy-module');
@@ -337,34 +180,13 @@ describe('`.plugin()`', function() {
 				spyModule.resetHistory();
 			});
 
-			it('calls plugin', function() {
-				expect(this.plugin).to.be.calledOnce;
-			});
-
-			it('calls plugin with 2 args', function() {
-				expectCalledWithTwoArgs(this.plugin);
-			});
-
-			it('calls plugin with app', function() {
-				expectCalledWithFirstArg(this.plugin, this.app);
-			});
-
-			it('calls plugin with global options', function() {
-				expectCalledWithSecondArg(this.plugin, this.globalOptions);
-			});
-
-			it('records plugin return value in plugins object', function() {
-				expect(this.app.plugins.spyModule).to.equal(this.res);
-			});
-
-			it('returns `app` for chaining', function() {
-				expect(this.ret).to.equal(this.app);
-			});
+			pluginTests({localOptions: false, skipEmptyObject: true});
 		});
 
 		describe('with PLUGIN_PREFIX defined', function() {
 			beforeEach(function() {
-				this.app = new Pluggi({'module': this.globalOptions});
+				this.pluginName = 'module';
+				this.app = new Pluggi({[this.pluginName]: this.globalOptions});
 				this.app[Pluggi.PLUGIN_PREFIX] = 'spy';
 				this.res = spyModule.spyModuleReturnValue;
 				this.plugin = spyModule;
@@ -375,32 +197,72 @@ describe('`.plugin()`', function() {
 				spyModule.resetHistory();
 			});
 
-			it('calls plugin', function() {
-				expect(this.plugin).to.be.calledOnce;
-			});
-
-			it('calls plugin with 2 args', function() {
-				expectCalledWithTwoArgs(this.plugin);
-			});
-
-			it('calls plugin with app', function() {
-				expectCalledWithFirstArg(this.plugin, this.app);
-			});
-
-			it('calls plugin with global options', function() {
-				expectCalledWithSecondArg(this.plugin, this.globalOptions);
-			});
-
-			it('records plugin return value in plugins object', function() {
-				expect(this.app.plugins.module).to.equal(this.res);
-			});
-
-			it('returns `app` for chaining', function() {
-				expect(this.ret).to.equal(this.app);
-			});
+			pluginTests({localOptions: false, skipEmptyObject: true});
 		});
 	});
 });
+
+function runTests(makePluginArgs, opts) {
+	let app, pluginName, pluginName2, globalOptions, options, res, plugin, ret;
+	beforeEach(function() {
+		({pluginName, pluginName2, globalOptions, options, res, plugin} = Object.assign({
+			pluginName: 'plugName',
+			pluginName2: 'plugName2',
+			globalOptions: {globalOpt: 1},
+			options: {localOpt: 2},
+			res: {resProp: 3},
+			plugin: sinon.fake.returns(res)
+		}, opts));
+
+		app = new Pluggi({[pluginName]: globalOptions});
+		const args = makePluginArgs(app, pluginName, plugin, options);
+		app.plugin.apply(app, args);
+	});
+}
+
+function pluginTests(options) {
+	if (!options) options = {};
+
+	it('calls plugin', function() {
+		expect(this.plugin).to.be.calledOnce;
+	});
+
+	it('calls plugin with 2 args', function() {
+		expectCalledWithTwoArgs(this.plugin);
+	});
+
+	it('calls plugin with app', function() {
+		expectCalledWithFirstArg(this.plugin, this.app);
+	});
+
+	if (options.localOptions) {
+		it('calls plugin with provided options', function() {
+			expectCalledWithSecondArg(this.plugin, sinon.match(this.options));
+		});
+
+		it('calls plugin with global options', function() {
+			expectCalledWithSecondArg(this.plugin, sinon.match(this.globalOptions));
+		});
+	} else {
+		it('calls plugin with global options', function() {
+			expectCalledWithSecondArg(this.plugin, this.globalOptions);
+		});
+	}
+
+	it('records plugin return value in plugins object', function() {
+		expect(this.app.plugins[this.pluginName]).to.equal(this.res);
+	});
+
+	if (!options.skipEmptyObject) {
+		it('records empty object in plugins object when plugin returns undefined', function() {
+			expect(this.app.plugins[this.pluginName2]).to.deep.equal({});
+		});
+	}
+
+	it('returns `app` for chaining', function() {
+		expect(this.ret).to.equal(this.app);
+	});
+}
 
 /*
  * Helper functions
